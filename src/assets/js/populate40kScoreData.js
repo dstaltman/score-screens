e9becd57752ff2b700977be3caef862c32a86c34
Dru Staltman <dstaltman@gmail.com>
2023-12-05 06:57:22 -0800

    Update populate40kScoreData.js
    Figure out total score from adding primary and secondary values

--- src/assets/js/populate40kScoreData.js
+++ src/assets/js/populate40kScoreData.js
@@ -52,8 +52,10 @@ export function populate40kScoreData(data) {
     // We'll put data from the json file in the right spots
     // We'll also process any information that should be computed. Such as Total Score
     playerNames.forEach(playerString  => {
-        // totalScore updated per player
+        // primaries, secondaries, and total score updated per player
         var totalScore = 0;
+        var primaryTotal = 0;
+        var secondaryTotal = 0;
 
         topLevelNames.forEach(dataLabelName => {
             var id = "#" + playerString + capitalize(dataLabelName);
@@ -67,7 +69,12 @@ export function populate40kScoreData(data) {
             // Add up the score if we this field has 'Score' in the name
             if (dataLabelName.includes("Score")) {
                 if (!isNaN(data[playerString][dataLabelName])) {
-                    totalScore += parseInt(data[playerString][dataLabelName]);
+                    if (dataLabelName.includes('primary')) {
+                        primaryTotal += parseInt(data[playerString][dataLabelName]);
+                    }
+                    else if(dataLabelName.includes('secondary')) {
+                        secondaryTotal += parseInt(data[playerString][dataLabelName]);
+                    }
                 }
             }
         });
@@ -85,12 +92,26 @@ export function populate40kScoreData(data) {
                 // Add up the score if we this field has 'Score' in the name
                 if (roundName.includes("Score")) {
                     if (!isNaN(roundData[roundName])) {
-                        totalScore += parseInt(roundData[roundName]);
+                        if (roundName.includes('primary')) {
+                            primaryTotal += parseInt(roundData[roundName]);
+                        }
+                        else if (roundName.includes('secondary')) {
+                            secondaryTotal += parseInt(roundData[roundName]);
+                        }
                     }
                 }
             });
             roundNumber++;
         }
+
+        // Cap primaries and secondaries at 45 points per how scoring works
+        if (primaryTotal > 45) {
+            primaryTotal = 45;
+        }
+        if (secondaryTotal > 45) {
+            secondaryTotal = 45;
+        }
+        totalScore = primaryTotal + secondaryTotal;
 
         // Update the total score tag
         var id = "#" + playerString + capitalize(totalScoreTopName);
